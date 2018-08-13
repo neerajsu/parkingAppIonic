fc9a7e47ba699db63444f1adf46cf36785e9d0ab
neerajsu <neeraj.subramaniyan@gmail.com>
2018-08-13 14:31:25 -0700

    UI changes to map search bar
    Map width uses full viewport and height shrinks after user clicks one of the parking lot markers to show details card below.

--- src/pages/find-parking/find-lots/find-lots.component.ts
+++ src/pages/find-parking/find-lots/find-lots.component.ts
@@ -31,88 +31,89 @@ import { AutocompletePage } from '../autocomplete/autocomplete.component';
 export class FindLotsComponent {
 	buildingsList = [
 		{
-			code : 123,
-			name : "Building A"
+			code: 123,
+			name: "Building A"
 		},
 		{
-			code : 124,
-			name : "Building B"
+			code: 124,
+			name: "Building B"
 		}
 	];
 	latitude: number = 47.556041;
 	longitude: number = -122.04965;
 	buildingSelection: any = {};
-	pages: Array<{title: string, component: any, icon: string, note: string, params?: any}>;
+	pages: Array<{ title: string, component: any, icon: string, note: string, params?: any }>;
 	deviceHeight: number;
 	deviceWidth: number;
 	mapsHeight: number;
 	mapsCSSHeight: String;
 	zoom: number = 15;
-	selectedMarker:any;
-	address:any;
-	
-  
-  // Google Map center
-  
-  
-  markers = [
-	  {
-		  latitude: 47.55627995194901,
-		  longitude: -122.04789928036918,
-		  label: "Lot A",
-		  address: "22010 Southeast 51st Street, Issaquah, Washington 98027, United States",
-		  workingHours: "6:00 AM to 11:00 PM",
-		  phoneNumber: "573-232-4197",
-		  email: "abcd@gmail.com",
-	  },
-	  {
-		  latitude: 47.5540467199795,
-		  longitude: -122.04678348141898,
-		  label: "Lot B",
-		  address: "5150 220th Avenue Southeast, Issaquah, Washington 98029, United States",
-		  workingHours: "6:00 AM to 11:00 PM",
-		  phoneNumber: "573-222-4197",
-		  email: "defd@gmail.com",
-	  },
-	  {
-		  latitude: 47.55700089607407,
-		  longitude: -122.05015233594169,
-		  label: "Lot C",
-		  address: "21930 Southeast 51st Street, Issaquah, Washington 98027, United States",
-		  workingHours: "6:00 AM to 11:00 PM",
-		  phoneNumber: "273-222-4197",
-		  email: "dsdgasffd@gmail.com",
-	  }
-  ]
+	selectedMarker: any;
+	address: any;
+
+
+	// Google Map center
+
+
+	markers = [
+		{
+			latitude: 47.55627995194901,
+			longitude: -122.04789928036918,
+			label: "Lot A",
+			address: "22010 Southeast 51st Street, Issaquah, Washington 98027, United States",
+			workingHours: "6:00 AM to 11:00 PM",
+			phoneNumber: "573-232-4197",
+			email: "abcd@gmail.com",
+		},
+		{
+			latitude: 47.5540467199795,
+			longitude: -122.04678348141898,
+			label: "Lot B",
+			address: "5150 220th Avenue Southeast, Issaquah, Washington 98029, United States",
+			workingHours: "6:00 AM to 11:00 PM",
+			phoneNumber: "573-222-4197",
+			email: "defd@gmail.com",
+		},
+		{
+			latitude: 47.55700089607407,
+			longitude: -122.05015233594169,
+			label: "Lot C",
+			address: "21930 Southeast 51st Street, Issaquah, Washington 98027, United States",
+			workingHours: "6:00 AM to 11:00 PM",
+			phoneNumber: "273-222-4197",
+			email: "dsdgasffd@gmail.com",
+		}
+	]
 
 	constructor(
 		private navController: NavController,
 		private menuController: MenuController,
 		private events: Events,
-		private modalCtrl:ModalController,
-		public platform: Platform){
-			this.deviceHeight = platform.height();
-			this.deviceWidth = platform.width();
-			this.address = {
-				'place': ''
-			};
-		}
+		private modalCtrl: ModalController,
+		public platform: Platform) {
+		this.deviceHeight = platform.height();
+		this.deviceWidth = platform.width();
+		this.address = {
+			'place': ''
+		};
+	}
 
 	ngOnInit() {
-		this.setMapsHeight();
+		this.setMapsHeight(100);
 		this.addBlueIconURLToMarkerArray();
 	}
 
 	markerClicked(marker: any, i: number) {
+		this.setMapsHeight(45);
 		this.selectedMarker = marker;
 		for (let index = 0; index < this.markers.length; index++) {
-			if(index == i){
+			if (index == i) {
 				this.markers[index]["iconUrl"] = 'http://maps.google.com/mapfiles/ms/icons/red.png';
 			} else {
 				this.markers[index]["iconUrl"] = 'http://maps.google.com/mapfiles/ms/icons/blue.png';
 			}
-		} 
-		
+		}
+
 	}
 
 	private addBlueIconURLToMarkerArray() {
@@ -120,9 +121,10 @@ export class FindLotsComponent {
 			element["iconUrl"] = 'http://maps.google.com/mapfiles/ms/icons/blue.png';
 		});
 	}
+	
 
-	setMapsHeight() {
-		this.mapsHeight = (this.deviceHeight * 45)/100;
+	setMapsHeight(percentage:number) {
+		this.mapsHeight = (this.deviceHeight * percentage) / 100;
 		this.mapsCSSHeight = this.mapsHeight + "px";
 	}
 
@@ -132,19 +134,21 @@ export class FindLotsComponent {
 		});
 	}
 
-	selectBuilding(buildingSelection : any) {
+	selectBuilding(buildingSelection: any) {
 		//Do something with updating drop downs/values based on this selection
 	}
 
-	showAddressModal () {
+	showAddressModal() {
 		let modal = this.modalCtrl.create(AutocompletePage);
 		let me = this;
 		modal.onDidDismiss(data => {
-		  this.address.place = data.place;
-		  this.latitude = data.latitude;
-		  this.longitude = data.longitude;
+			if (data) {
+				this.address.place = data.place;
+				this.latitude = data.latitude;
+				this.longitude = data.longitude;
+			}
 		});
 		modal.present();
-	  }
+	}
 
 }
